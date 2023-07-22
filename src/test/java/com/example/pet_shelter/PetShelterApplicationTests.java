52ae67629dac8a156c9a54527647420f1ef58399
93294 <9329496@mail.ru>
2023-07-22 22:51:08 +0300

    Initial commit

---
+++ src/test/java/com/example/pet_shelter/PetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
